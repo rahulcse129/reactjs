3ddcbae290334eb552484aa6f36f2134f3a66369
rahul1 <rahulcse129@gmail.com>
2025-05-19 12:31:06 +0530

    react is learning Stage 2

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./hello"
+function App() {
+
+
+  return (
+    <>
+   <Hello/>
+   <h1>jfoajosjfjsa</h1>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/hello.jsx
@@ -0,0 +1,8 @@
+
+function Hello(){
+    return (
+        <h3>HELLOOO</h3>
+    )
+}
+
+export default Hello
